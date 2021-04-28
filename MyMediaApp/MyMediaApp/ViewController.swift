1772c1eb2a1f5ff7db78bb4973476ebf181e1799
harsh aggarwal <harsh.aggarwal@tothenew.com>
2021-04-28 14:51:57 +0530

    initial commit with git ignore

---
+++ MyMediaApp/MyMediaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMediaApp
+//
+//  Created by harsh_TTN on 28/04/21.
+//  Copyright © 2021 harsh_TTN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
